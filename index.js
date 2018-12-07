d864bd616d9e0dfc1f5553e43fca08adcc266d61
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-12-08 02:34:46 +0530

    updating

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var serand = require('serand');
 
 dust.loadSource(dust.compile(require('./template'), 'accounts-authorize'));
 
-module.exports = function (ctx, sandbox, options, done) {
+module.exports = function (ctx, container, options, done) {
+    var sandbox = container.sandbox;
     dust.render('accounts-authorize', options, function (err, out) {
         if (err) {
             return done(err);
